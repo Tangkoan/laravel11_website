edb37bc8c51d04a69bd2115615a79e3ef1a28641
Tangkoan <kuytangkoan@gmail.com>
2024-11-11 15:32:55 +0700

    component

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');  // admin = floder , index name file index.blade.php
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {

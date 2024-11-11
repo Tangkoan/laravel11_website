e7134ff41852f538a8b98678073a53f0685b4307
Tangkoan <kuytangkoan@gmail.com>
2024-11-11 16:57:06 +0700

    34

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -56,4 +56,128 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<!doctype html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8" />
+        <title>Register | Admin</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesdesign" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ asset('Backend/assets/images/favicon.ico')}}">
+
+        <!-- Bootstrap Css -->
+        <link href="{{ asset('Backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{ asset('Backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{ asset('Backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body class="auth-body-bg">
+        <div class="bg-overlay"></div>
+        <div class="wrapper-page">
+            <div class="container-fluid p-0">
+                <div class="card">
+                    <div class="card-body">
+
+                        <div class="text-center mt-4">
+                            <div class="mb-3">
+                                <a href="index.html" class="auth-logo">
+                                    <img src="{{ asset('Backend/assets/images/logo-dark.png')}}" height="30" class="logo-dark mx-auto" alt="">
+                                    <img src="{{ asset('Backend/assets/images/logo-light.png')}}" height="30" class="logo-light mx-auto" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                        <h4 class="text-muted text-center font-size-18"><b>Register</b></h4>
+
+                        <div class="p-3">
+                            {{-- <form class="form-horizontal mt-3" action="index.html"> --}}
+                                <form class="form-horizontal mt-3" method="POST" action="{{ route('register') }}">
+                                    @csrf
+                                {{-- unsername --}}
+                                <div class="form-group mb-3 row">
+                                    <div class="col-12">
+                                        <input class="form-control" id="username" name="username" type="text" required="" placeholder="Username">
+                                    </div>
+                                </div>
+
+                                {{-- name --}}
+                                <div class="form-group mb-3 row">
+                                    <div class="col-12">
+                                        <input class="form-control" id="name" name="name" type="text" required="" placeholder="Name">
+                                    </div>
+                                </div>
+
+                                {{-- email --}}
+                                <div class="form-group mb-3 row">
+                                    <div class="col-12">
+                                        <input class="form-control" name="email" id="email" type="email" required="" placeholder="Email">
+                                    </div>
+                                </div>
+
+                                {{-- password --}}
+                                <div class="form-group mb-3 row">
+                                    <div class="col-12">
+                                        <input class="form-control" id="password" name="password" type="password" required="" placeholder="Password">
+                                    </div>
+                                </div>
+
+                                {{-- Confrim password --}}
+                                <div class="form-group mb-3 row">
+                                    <div class="col-12">
+                                        <input class="form-control" id="password_confirmation" name="password_confirmation" type="password" required="" placeholder="Password Confirm">
+                                    </div>
+                                </div>
+
+                                <div class="form-group mb-3 row">
+                                    <div class="col-12">
+                                        <div class="custom-control custom-checkbox">
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group text-center row mt-3 pt-1">
+                                    <div class="col-12">
+                                        <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Register</button>
+                                    </div>
+                                </div>
+
+                                <div class="form-group mt-2 mb-0 row">
+                                    <div class="col-12 mt-3 text-center">
+                                        <a href="{{route('login')}}" class="text-muted">Already have account?</a>
+                                    </div>
+                                </div>
+                            </form>
+                            <!-- end form -->
+                        </div>
+                    </div>
+                    <!-- end cardbody -->
+                </div>
+                <!-- end card -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end -->
+
+
+        <!-- JAVASCRIPT -->
+        <script src="{{ asset('Backend/assets/libs/node-waves/waves.min.js')}}"></script>
+        <script src="{{ asset('Backend/assets/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{ asset('Backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{ asset('Backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+        <script src="{{ asset('Backend/assets/libs/simplebar/simplebar.min.js"></script>
+
+        <script src="assets/js/app.js"></script>
+
+    </body>
+</html>
+

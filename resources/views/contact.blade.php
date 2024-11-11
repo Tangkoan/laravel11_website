cf0e089a682866e77c3cf8aa29413cdfab80b05f
Tangkoan <kuytangkoan@gmail.com>
2024-11-11 13:50:59 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Page</title>
+</head>
+<body>
+    <h1>Welcome TO Contact Page</h1>
+    <Button>
+        {{-- <a href="/about">About</a> --}}
+        <a href="{{route('about.page')}}">About</a>
+    </Button>
+</body>
+</html>
